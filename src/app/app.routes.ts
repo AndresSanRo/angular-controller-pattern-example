a2db2a90435bc9084b12e889b568b4d52b013e0c
Andrés Sánchez Robleño <andres.sanche@encamina.com>
2024-01-31 08:53:56 +0100

    feat: add components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'dog', loadComponent: () => import('./feature/dog/dog.component').then(m => m.DogComponent) },
+    { path: 'bird', loadComponent: () => import('./feature/bird/bird.component').then(m => m.BirdComponent)},
+    { path: 'fish', loadComponent: () => import('./feature/fish/fish.component').then(m => m.FishComponent)}
+];
---
+++ src/app/feature/bird/bird.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bird',
+  standalone: true,
+  imports: [],
+  templateUrl: './bird.component.html',
+  styleUrl: './bird.component.scss'
+})
+export class BirdComponent {
+
+}
